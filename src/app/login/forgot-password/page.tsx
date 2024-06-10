7c9571c6d3110e1f14f8d0a418aad746561be673
Akshit-02 <akshitmurarka2002@gmail.com>
2024-06-10 21:33:30 +0530

    created login page

---
+++ src/app/login/forgot-password/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import ForgotPasswordForm from "@/container/ForgotPassword/ForgotPassword";
+import React from "react";
+
+const ForgotPasswordPage = () => {
+  return (
+    <div className="screenWidth">
+      <ForgotPasswordForm />
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/container/LoginForm/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="screenWidth">
+      <LoginForm />
+    </div>
+  );
+}
