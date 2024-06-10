262124c4be182621c4315d129201aae114ca7de6
Akshit-02 <akshitmurarka2002@gmail.com>
2024-06-10 21:33:59 +0530

    created register route

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import RegistrationForm from "@/container/RegistrationForm/RegistrationForm";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div className="screenWidth">
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
