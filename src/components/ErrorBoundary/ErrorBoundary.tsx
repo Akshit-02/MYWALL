880d4cad580130eaf727ab4bc92290d212176d42
Akshit-02 <akshitmurarka2002@gmail.com>
2024-06-14 18:33:40 +0530

    added error boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { ErrorInfo } from "react";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log("error boundary");
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
